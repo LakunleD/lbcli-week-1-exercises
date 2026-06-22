fae0ef4f062ca8131ee517b1f45d5b9a5e59e599
Olakunle Dosunmu <olakunledosunmu@gmail.com>
2026-06-22 21:20:09 +0100

    run test

--- submission/04.sh
+++ submission/04.sh
@@ -1,2 +1,2 @@
 # Create a wallet with the name "builderswallet".
-bitcoin-cli -regtest -named createwallet wallet_name="builderswallet" descriptors=false
+bitcoin-cli -regtest -named createwallet wallet_name="builderswallet"
